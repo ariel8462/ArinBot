fb7651a81ca90ffd270ed97ff37dde969f0476f0
ariel8462 <ariel.amromin@gmail.com>
2021-12-31 11:39:29 +0200

    deleted some files, moved functions to other files

--- ArinBot/modules/misc.py
+++ ArinBot/modules/misc.py
@@ -2,6 +2,7 @@ import discord
 from discord.ext import commands
 from discord.errors import Forbidden
 from config import Config
+import time
 
 class Misc(commands.Cog):
     def __init__(self, client: commands.Bot):
@@ -38,6 +39,18 @@ class Misc(commands.Cog):
             return
 
         await context.reply(f"Changed the nickname of {member.name}")
+
+    @commands.command(aliases=["count", "member_count"])
+    async def members(self,  context: commands.Context):
+        await context.send(f"The server has {context.guild.member_count} members")
+
+    @commands.command()
+    async def ping(self, context: commands.Context) -> None:
+        start_time = time.time()
+        message = await context.send("Pinging...")
+        end_time = time.time()
+        ping_time = round((end_time - start_time) * 1000, 3)
+        await message.edit(content=str(ping_time) + " ms")
 
 
 def setup(client: commands.Bot):
